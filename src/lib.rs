fd2a3004f2d26a1a0fb8ba3756c218626a3566de
André Oliveira <p32blo@gmail.com>
2016-08-10 00:11:10 +0100

    Start to model the machine

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,29 @@
 
-struct VM {
+use std::path::Path;
+
+
+enum Operand {
+    Integer(i32),
+    Float(f32),
+    Address(usize),
+}
+
+struct Machine {
+    /// Stack Pointer
+    sp: usize,
+    /// Frame Pointer
+    fp: usize,
+    /// Program Counter
+    pc: usize,
+    /// Global Variables Base Address
+    gp: usize,
+    /// Operand Stack
+    operands: Vec<Operand>,
+    /// Call Stack (instruction address, frame pointer)
+    stack: (usize, usize)
+}
+
+pub fn start <P: AsRef<Path>>(path: P) {
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,11 @@
 extern crate vm;
 
 fn main() {
-    println!("Hello World!");
+
+    let mut args = std::env::args().skip(1);
+
+    match &args.len() {
+        &1 => vm::start(args.next().unwrap()),
+        _ => println!("Provide a file for execution")
+    }
 }
