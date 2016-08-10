e920520aff9aa5f86c8f10154f8052e32a1f33f4
André Oliveira <p32blo@gmail.com>
2016-08-10 13:12:23 +0100

    Implement the first instructions
    - 'pushi'
    - 'pushn'
    - 'start'

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 
 use std::io;
+use std::fmt;
 use std::path::Path;
 use std::fs::File;
 use std::io::Read;
+use std::collections::HashMap;
 
 #[derive(Debug)]
 enum Operand {
@@ -11,7 +13,7 @@ enum Operand {
     Address(usize),
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug,Default)]
 struct Machine {
     /// Stack Pointer
     sp: usize,
@@ -22,11 +24,13 @@ struct Machine {
     /// Global Variables Base Address
     gp: usize,
     /// Operand Stack
-    operands: Vec<Operand>,
+    stack: Vec<Operand>,
     /// Call Stack (instruction address, frame pointer)
-    stack: (usize, usize),
+    call_stack: Vec<(usize, usize)>,
     /// Code
     code: Vec<String>,
+    /// Label Map
+    labels: HashMap<String, usize>,
 }
 
 impl Machine {
@@ -38,29 +42,56 @@ impl Machine {
         let mut f = try!(File::open(path));
         let mut buffer = String::new();
         try!(f.read_to_string(&mut buffer));
-        self.code = buffer.lines().map(|x| x.trim().to_string()).collect::<Vec<String>>();
+        self.code = buffer.lines().map(|x| x.trim().to_lowercase().to_string()).collect();
         Ok(())
     }
 
-    fn run(&self) {
+    fn run(&mut self) {
         loop {
             self.run_instruction();
             println!("{:#?}", *self);
         }
     }
 
-    fn run_instruction(&self) {
+    fn run_instruction(&mut self) {
         let (inst, val) = self.get_instruction();
 
-        match inst {
-            _ => panic!(format!("Instruction not found: {}", inst))
+
+        if inst.contains(":") {
+            let pc = self.pc;
+            self.label(&inst, pc);
+        } else {
+            match inst.as_ref() {
+                "pushi" => self.pushi(&val.unwrap()),
+                "pushn" => self.pushn(&val.unwrap()),
+                "start" => self.start(),
+                _ => panic!(format!("Instruction not found: {}", inst)),
+            }
+        }
+        self.pc += 1;
+    }
+
+    fn get_instruction(&self) -> (String, Option<String>) {
+        let ref inst = self.code[self.pc];
+        let mut split = inst.split_whitespace().map(|x| x.to_string());
+        (split.next().unwrap(), split.next())
+    }
+
+    fn pushi(&mut self, val: &str) {
+        self.stack.push(Operand::Integer(val.parse().unwrap()));
+        self.sp += 1;
+    }
+
+    fn pushn(&mut self, val: &str) {
+        for _ in 0..val.parse().unwrap() {
+            self.pushi("0");
         }
     }
 
-    fn get_instruction(&self) -> (String, String) {
-        let ref inst = self.code[self.pc];
-        let mut split = inst.split_whitespace().map(|x| x.to_string());
-        (split.next().unwrap(), split.next().unwrap())
+    fn start(&self) {}
+
+    fn label(&mut self, mat: &str, pc: usize) {
+        self.labels.insert(mat.to_string(), pc);
     }
 }
 
@@ -68,7 +99,7 @@ impl Machine {
 pub fn start<P: AsRef<Path>>(path: P) -> io::Result<()> {
     let mut m = Machine::new();
     try!(m.load(path));
-    //println!("{:#?}", m);
+    // println!("{:#?}", m);
     m.run();
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ fn main() {
 
     let mut args = std::env::args().skip(1);
 
-    match &args.len() {
-        &1 => vm::start(args.next().unwrap()).unwrap(),
+    match args.by_ref().len() {
+        1 => vm::start(args.next().unwrap()).unwrap(),
         _ => println!("Provide a file for execution"),
     }
 }
