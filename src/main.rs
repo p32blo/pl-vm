36c38b5e35a01323a1d53c91e982c7da7a875dad
André Oliveira <p32blo@gmail.com>
2016-08-21 16:48:56 +0100

    Rework error handling

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 
 extern crate vm;
 
+use std::error::Error;
+
 fn main() {
 
     let mut args = std::env::args().skip(1);
 
     match args.by_ref().len() {
-        1 => vm::start(args.next().unwrap()).unwrap(),
-        _ => println!("Provide a file for execution"),
+        1 => {
+            let file = args.next().unwrap();
+            if let Err(e) = vm::start(&file) {
+                println!("error: {}", e.description());
+            }
+        }
+        _ => println!("error: no file provided"),
     }
 }
