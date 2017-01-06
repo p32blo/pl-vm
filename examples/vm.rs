1e2fd7bb370d01b6ab727bf4119e4b3c3a9ec997
p32blo <p32blo@gmail.com>
2017-01-06 16:25:53 +0000

    Disable backtrace support in error-chain
    - The error handling was bringing to many dependencies, incresing the build time
    - Simplify usage example, and remove 'ansi_term' as a dependencie

--- examples/vm.rs
+++ examples/vm.rs
@@ -1,34 +1,15 @@
 
 extern crate vm;
-extern crate ansi_term;
-
-use std::process;
-use ansi_term::Colour::{Red, Blue};
 
 fn main() {
-    let args = std::env::args().skip(1);
-
-    for file in args {
-
-        println!();
-        println!("{} {}", Blue.paint("Begin Execution:"), file);
-
+    // There is a file argument
+    if let Some(file) = std::env::args().skip(1).next() {
+        // There are errors running the vm
         if let Err(ref e) = vm::start(&file) {
-            println!("{} {}", Red.paint("error:"), e);
-
+            println!("error: {}", e);
             for e in e.iter().skip(1) {
-                println!("{} {}", Red.paint("caused by:"), e);
-            }
-
-            // The backtrace is not always generated. Try to run this example
-            // with `RUST_BACKTRACE=1`.
-            if let Some(backtrace) = e.backtrace() {
-                println!("backtrace: {:?}", backtrace);
+                println!("caused by: {}", e);
             }
         }
-
-        println!();
-        println!("{} {}", Blue.paint("End Execution:"), file);
     }
-    process::exit(0);
 }
