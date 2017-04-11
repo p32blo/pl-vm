d667815bfd971c8c50b045c02e776d99d0eaab07
André Oliveira <p32blo@gmail.com>
2017-04-11 04:22:07 +0100

    Remove previous instruction parsing strategy

--- src/instructions.rs
+++ src/instructions.rs
@@ -4,15 +4,10 @@
 extern crate unescape;
 
 use std::fmt;
-use std::str::FromStr;
-
-use errors::*;
 
 /// Possible `vm` instructions
 #[derive(Clone, Debug)]
 pub enum Instruction {
-    //Check(i32, i32),
-    //Pushf(f32),
     Pushi(i32),
     Pushn(i32),
     Pushg(usize),
@@ -47,64 +42,9 @@ pub enum Instruction {
     Err(String),
 }
 
-
-impl FromStr for Instruction {
-    type Err = Error;
-    fn from_str(instr: &str) -> Result<Instruction> {
-        let (inst, val) = Self::decode(instr);
-
-        let val_s = |val: Option<String>| val.ok_or_else(|| "No value found".into());
-        let val_i = |val_s: Result<String>| {
-            val_s.and_then(|x| x.parse().chain_err(|| "value is not a integer"))
-        };
-        let val_u = |val_s: Result<String>| {
-            val_s.and_then(|x| x.parse().chain_err(|| "value is not a positive integer"))
-        };
-
-        let res = match inst.as_ref() {
-            "pushi" => Instruction::Pushi(val_i(val_s(val))?),
-            "pushn" => Instruction::Pushn(val_i(val_s(val))?),
-            "pushg" => Instruction::Pushg(val_u(val_s(val))?),
-            "pushs" => Instruction::Pushs(val_s(val).and_then(|x| Self::remove_quotes(&x))?),
-            "pusha" => Instruction::Pusha(val_s(val)?),
-            "pushgp" => Instruction::Pushgp,
-            "call" => Instruction::Call,
-            "return" => Instruction::Return,
-            "start" => Instruction::Start,
-            "nop" => Instruction::Nop,
-            "stop" => Instruction::Stop,
-            "loadn" => Instruction::Loadn,
-            "writei" => Instruction::Writei,
-            "writes" => Instruction::Writes,
-            "read" => Instruction::Read,
-            "atoi" => Instruction::Atoi,
-            "padd" => Instruction::Padd,
-            "add" => Instruction::Add,
-            "mul" => Instruction::Mul,
-            "div" => Instruction::Div,
-            "mod" => Instruction::Mod,
-            "storeg" => Instruction::Storeg(val_u(val_s(val))?),
-            "storen" => Instruction::Storen,
-            "equal" => Instruction::Equal,
-            "inf" => Instruction::Inf,
-            "infeq" => Instruction::Infeq,
-            "sup" => Instruction::Sup,
-            "supeq" => Instruction::Supeq,
-            "jump" => Instruction::Jump(val_s(val)?),
-            "jz" => Instruction::Jz(val_s(val)?),
-            "err" => Instruction::Err(val_s(val).and_then(|x| Self::remove_quotes(&x))?),
-            _ => bail!(format!("Instruction not found '{}'", inst)),
-        };
-        Ok(res)
-    }
-}
-
-
 impl fmt::Display for Instruction {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            //Instruction::Check(ref val_a, ref val_b) => write!(f, "check {} {}", val_a, val_b),
-            //Instruction::Pushf(ref val) => write!(f, "pushf {}", val),
             Instruction::Pushi(ref val) => write!(f, "pushi {}", val),
             Instruction::Pushn(ref val) => write!(f, "pushn {}", val),
             Instruction::Pushg(ref val) => write!(f, "pushg {}", val),
@@ -141,28 +81,7 @@ impl fmt::Display for Instruction {
     }
 }
 
-
 impl Instruction {
-    fn decode(inst_ref: &str) -> (String, Option<String>) {
-        let find = inst_ref.find(' ');
-        match find {
-            None => (inst_ref.to_string(), None),
-            Some(f) => {
-                let (inst, val) = inst_ref.split_at(f);
-                (inst.to_string(), Some(val.trim().to_string()))
-            }
-        }
-    }
-
-    fn remove_quotes(string: &str) -> Result<String> {
-        let s = string.split('"').collect::<Vec<_>>();
-        if s.len() == 3 && s.starts_with(&[""]) && s.ends_with(&[""]) {
-            unescape::unescape(s[1]).ok_or_else(|| "Wrong string escape sequence".into())
-        } else {
-            bail!("Value is not a String")
-        }
-    }
-
     pub fn write_ln(&self) {
         match *self {
             Instruction::Writei | Instruction::Writes => println!(),
